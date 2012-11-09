9872dda24d48d290efa83129a7970a481758278a
rafa <pr@pr.pr>
2012-11-09 18:01:26 +0100

    ejercicios

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,6 +80,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -115,5 +125,7 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+
 
 end
