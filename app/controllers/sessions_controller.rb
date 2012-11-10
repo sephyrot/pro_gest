62b3a265b7c0ef33c4f7218327be13b59c93608a
rafa <pr@pr.pr>
2012-11-10 21:26:17 +0100

    control de usuarios

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:session][:email].downcase)
-	if user && user.authenticate(params[:session][:password])
-	  sign_in user #tendremos que crear este metodo en un helper
-      redirect_to user
-	else
-	  flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
-	end
+    end
   end
 
   def destroy
