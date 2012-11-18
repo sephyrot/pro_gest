cadfc4d77bd884224ba680d72ef723907d12bcb4
rafa <pr@pr.pr>
2012-11-18 13:43:12 +0100

    se estructura el codigo para los colores, y se elimina la opcion del menu para las solicitudes

--- app/helpers/sol_servicios_helper.rb
+++ app/helpers/sol_servicios_helper.rb
@@ -1,2 +1,27 @@
 module SolServiciosHelper
+	#Define el color de las solicitudes dependiendo del tiempo que le quede respecto a la fecha de entrega
+	def color (sol_servicio)
+		@clase = ""
+		
+		if !sol_servicio.fecha_entrega.nil?
+		    @verde = (sol_servicio.fecha_entrega > Date.today.months_since(1))
+		    @amarillo = (Date.today.next_week(:friday).to_datetime < sol_servicio.fecha_entrega) && ( sol_servicio.fecha_entrega < Date.today.months_since(1)) 
+		    @rojo = (Date.today.next_week(:friday).to_datetime > sol_servicio.fecha_entrega)  
+
+	        if @verde
+	            @clase = "rowsubmes"
+	        else
+	            if @amarillo
+	                @clase = "rowsubsem"
+	            else
+	                if @rojo
+	                    @clase = "rowsubdia"
+	                end
+	            end
+
+	        end
+	    end
+	
+	end
+
 end
